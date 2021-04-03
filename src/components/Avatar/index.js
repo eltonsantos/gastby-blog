1ac35d130b8aae0eea2f2e0a94fd7bcacb1af39a
Elton Santos <eltonaxl@hotmail.com>
2021-04-03 08:33:32 -0300

    Almost finish

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,24 +1,18 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
 import * as S from "./styled"
 
 const Avatar = () => {
-  const { avatarImage } = useStaticQuery(
-    graphql`
-      query {
-        avatarImage: file(relativePath: { eq: "profile-photo.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 60) {
-              ...GatsbyImageSharpFluid_tracedSVG
-            }
-          }
-        }
-      }
-    `
+  return (
+    <S.AvatarWrapper>
+      <StaticImage
+        src="../../images/profile-photo.png"
+        alt="John Doe Avatar"
+        placeholder="blurred"
+      />
+    </S.AvatarWrapper>
   )
-
-  return <S.AvatarWrapper fluid={avatarImage.childImageSharp.fluid} />
 }
 
 export default Avatar
